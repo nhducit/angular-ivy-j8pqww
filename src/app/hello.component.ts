4ccfb747c92f7e9dc1a348edee12dc57bc4cdd80
nhducit <nhducit@gmail.com>
2022-06-11 00:20:43 +0700

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,42 +1,69 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { QueryClient } from 'react-query/core';
-import { QueryObserver } from 'react-query/lib/core';
+import { QueryClient, QueryObserver, QueryKey } from 'react-query/core';
+import { UseQueryOptions, UseQueryResult } from 'react-query/lib/reactjs/types';
+import { of, Subject } from 'rxjs';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
+  template: `<h1>Hello {{queryResult.data}}!</h1>`,
   styles: [`h1 { font-family: Lato; }`],
 })
 export class HelloComponent implements OnInit {
   @Input() name: string;
 
+  queryResult = null;
+
   ngOnInit() {
-    const queryClient = new QueryClient();
-    // create query options
-    const defaultedOptions = queryClient.defaultQueryOptions({
-      queryKey: ['test'],
-      queryFn: () => {
-        return 'hihi';
-      },
-      onSuccess: (data) => {
-        console.log('onSuccess', data);
-      },
-      onError: () => {
-        console.log('onError');
-      },
+    const queryResult$ = useQuery({});
+    queryResult$.subscribe((data) => {
+      console.log('queryResult----', data);
+      this.queryResult = data;
+    });
+  }
+}
+// singleton for entire app
+const queryClient = new QueryClient();
+
+function useQuery<
+  TQueryFnData = unknown,
+  TError = unknown,
+  TData = TQueryFnData,
+  TQueryKey extends QueryKey = QueryKey
+>(options: UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>): any {
+  const result$ = new Subject<any>();
+  // create query options
+  const defaultedOptions = queryClient.defaultQueryOptions({
+    queryKey: ['test'],
+    queryFn: () => {
+      return Promise.resolve('hihi');
+    },
+    onSuccess: (data) => {
+      console.log('onSuccess', data);
+    },
+    onError: () => {
+      console.log('onError');
+    },
+  });
+
+  // create new observer for the current query
+  const observer = new QueryObserver(queryClient, defaultedOptions);
+  observer
+    .fetchOptimistic(defaultedOptions)
+    .then(({ data }) => {
+      defaultedOptions.onSuccess?.(data as any);
+      defaultedOptions.onSettled?.(data, null);
+    })
+    .catch((error) => {
+      defaultedOptions.onError?.(error);
+      defaultedOptions.onSettled?.(undefined, error);
     });
 
-    // create new observer for the current query
-    const observer = new QueryObserver(queryClient, defaultedOptions);
-    observer
-      .fetchOptimistic(defaultedOptions)
-      .then(({ data }) => {
-        defaultedOptions.onSuccess?.(data as any);
-        defaultedOptions.onSettled?.(data, null);
-      })
-      .catch((error) => {
-        defaultedOptions.onError?.(error);
-        defaultedOptions.onSettled?.(undefined, error);
-      });
-  }
+  const result = observer.getOptimisticResult(defaultedOptions);
+  result$.next(result);
+
+  observer.subscribe((data) => {
+    result$.next(data);
+  });
+
+  return result$;
 }
